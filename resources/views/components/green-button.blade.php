df7ce1c2d95fa56b2e9a3f080760d84c5c00164e
Ibnul Huq <ibnul.huq@gmail.com>
2023-06-29 16:55:39 +0100

    add reusable button components

---
+++ resources/views/components/green-button.blade.php
@@ -0,0 +1,4 @@
+<button type="button" {{ $attributes->merge(['class' => 'focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800']) }} 
+    >{{ $slot }}
+</button>
+
