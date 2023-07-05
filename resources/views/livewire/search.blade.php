a60e583f740373040907f5f28da73fde75fb9743
Ibnul Huq <ibnul.huq@gmail.com>
2023-07-05 10:03:44 +0100

    remove unused components

--- resources/views/livewire/search.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <x-admin.livewire-search
-        class="my-5"    
-        wire:model="search"
-    />
-
-    @livewire('test',['searchResults' => $search])
-</div>
--- resources/views/livewire/test.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <p>{{ $searchResults }}</p>
-</div>
